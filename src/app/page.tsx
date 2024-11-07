2281a88a17ee9ab4ca06cbe3036a7245bee0668c
Shmuel Amir <shmulik5777@gmail.com>
2024-11-07 21:33:20 +0200

    scaffold UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,34 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/ImNm43RMASx2IFmXw1RMASx2uleEyrcpPO4jDU97bq5wCVLk",
+  "https://utfs.io/f/ImNm43RMASx2QUVreG2HGrgFC8stA1JqvwL5xb4ySeV7HXaE",
+  "https://utfs.io/f/ImNm43RMASx2IFmXw1RMASx2uleEyrcpPO4jDU97bq5wCVLk",
+  "https://utfs.io/f/ImNm43RMASx2VpP3RkwdLgkXeBisHtWq0KGIUjnJ4S9z1MEl",
+  "https://utfs.io/f/ImNm43RMASx2fMsocc2vh1yKuGlFIkV56XNjZ7oMbT8EQHWJ",
+  "https://utfs.io/f/ImNm43RMASx247gKacr9lGV1UkeYqoN72COL0tfwJrIEiaXh",
+  "https://utfs.io/f/ImNm43RMASx2vxVveFpDQRLCGe38qa47fbwuWmTsFB9HgAPO",
+  "https://utfs.io/f/ImNm43RMASx2JxSgKUExdBG2hiQI89eOl1EZ3DAb4YCvVtsP",
+  "https://utfs.io/f/ImNm43RMASx2NzMaItKA2QoyUMhgnzWP4iLxkRZclpqsKX8T",
+  "https://utfs.io/f/ImNm43RMASx24O3lIKr9lGV1UkeYqoN72COL0tfwJrIEiaXh",
+  "https://utfs.io/f/ImNm43RMASx22xNN4u0OP8WvJEBgKLueFymMtfwSjs0YC6bp",
+  "https://utfs.io/f/ImNm43RMASx249e1naIr9lGV1UkeYqoN72COL0tfwJrIEiaX",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {mockImages.map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} alt="" />
+          </div>
+        ))}
       </div>
     </main>
   );
