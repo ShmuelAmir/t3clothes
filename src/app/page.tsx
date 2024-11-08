430960dcca1f4c68b7fd263d869e1d9ccd48c4d8
Shmuel Amir <shmulik5777@gmail.com>
2024-11-08 13:55:57 +0200

    actually linking the db to UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,19 @@
-import Link from "next/link";
 import { db } from "~/server/db";
 
-const mockUrls = [
-  "https://utfs.io/f/ImNm43RMASx2IFmXw1RMASx2uleEyrcpPO4jDU97bq5wCVLk",
-  "https://utfs.io/f/ImNm43RMASx2QUVreG2HGrgFC8stA1JqvwL5xb4ySeV7HXaE",
-  "https://utfs.io/f/ImNm43RMASx2IFmXw1RMASx2uleEyrcpPO4jDU97bq5wCVLk",
-  "https://utfs.io/f/ImNm43RMASx2VpP3RkwdLgkXeBisHtWq0KGIUjnJ4S9z1MEl",
-  "https://utfs.io/f/ImNm43RMASx2fMsocc2vh1yKuGlFIkV56XNjZ7oMbT8EQHWJ",
-  "https://utfs.io/f/ImNm43RMASx247gKacr9lGV1UkeYqoN72COL0tfwJrIEiaXh",
-  "https://utfs.io/f/ImNm43RMASx2vxVveFpDQRLCGe38qa47fbwuWmTsFB9HgAPO",
-  "https://utfs.io/f/ImNm43RMASx2JxSgKUExdBG2hiQI89eOl1EZ3DAb4YCvVtsP",
-  "https://utfs.io/f/ImNm43RMASx2NzMaItKA2QoyUMhgnzWP4iLxkRZclpqsKX8T",
-  "https://utfs.io/f/ImNm43RMASx24O3lIKr9lGV1UkeYqoN72COL0tfwJrIEiaXh",
-  "https://utfs.io/f/ImNm43RMASx22xNN4u0OP8WvJEBgKLueFymMtfwSjs0YC6bp",
-  "https://utfs.io/f/ImNm43RMASx249e1naIr9lGV1UkeYqoN72COL0tfwJrIEiaX",
-];
-
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
+export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
 
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {mockImages.map((image) => (
-          <div key={image.id} className="w-48">
-            <img src={image.url} alt="" />
+        {images.map((image) => (
+          <div key={image.id} className="flex w-48 flex-col">
+            <img src={image.url} />
+            <p>{image.name}</p>
           </div>
         ))}
       </div>
