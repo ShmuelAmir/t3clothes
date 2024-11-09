19f82438af86835c153466aa2f946537c17aebba
Shmuel Amir <shmulik5777@gmail.com>
2024-11-09 20:14:04 +0200

    uploadthing added

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,14 +1,16 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import UploadButtonClient from "./upload-button-client";
 
 export function TopNav() {
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
       <div>Gallery</div>
-      <div>
+      <div className="flex items-center gap-4">
         <SignedOut>
           <SignInButton />
         </SignedOut>
         <SignedIn>
+          <UploadButtonClient/>
           <UserButton />
         </SignedIn>
       </div>
